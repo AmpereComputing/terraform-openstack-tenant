1b04708bf698d99dc2c50176ead9f9bd99a82449
Peter Pouliot <peter@pouliot.net>
2020-09-14 08:31:17 -0400

    Update role_assignment.tf

--- role_assignment.tf
+++ role_assignment.tf
@@ -1,3 +1,4 @@
+# OpenStack Role Assignment
 resource "openstack_identity_role_assignment_v3" "role_assignment_1" {
   user_id    = openstack_identity_user_v3.user_1.id
   project_id = openstack_identity_project_v3.project_1.id
