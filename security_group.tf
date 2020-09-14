8a385289c6baa7ff54179d10ae4e4c709d19e002
Peter Pouliot <peter@pouliot.net>
2020-09-14 08:32:46 -0400

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -2,35 +2,35 @@
 resource "openstack_compute_secgroup_v2" "secgroup_1" {
   name        = "${var.project_name}_security_group"
   description = "${var.project_name} security group"
-
+# SSH Port
   rule {
     from_port   = 22
     to_port     = 22
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
-
+# HTTP Port
   rule {
     from_port   = 80
     to_port     = 80
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
-
+# HTTPS Port
   rule {
     from_port   = 443
     to_port     = 443
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
-
+# 8080 for K8S mgmt
   rule {
     from_port   = 8080
     to_port     = 8080
     ip_protocol = "tcp"
     cidr        = "0.0.0.0/0"
   }
-
+# Allow ping
   rule {
     from_port   = -1
     to_port     = -1
