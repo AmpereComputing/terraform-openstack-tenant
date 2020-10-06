05112931ff83286bcb1c4e128445429fb38a212c
Peter Pouliot <peter@pouliot.net>
2020-10-06 12:58:24 +0000

    lines in variables.tf caused error

--- variables.tf
+++ variables.tf
@@ -6,25 +6,21 @@ variable "os_project_name" {
 variable "os_project_description" {
   description = "OpenStack Project description"
   type = string
-  default = "OpenStack Project: ${var.os_project_name}"
 }
 
 variable "os_role_name" {
   description = "OpenStack Role Name"
   type = string
-  default = "${var.os_project_name}_Admins"
 }
 
 variable "os_usernames" {
   description = "OpenStack Username"
   type = string
-  default = ["user1","user2"]
 }
 
 variable "os_user_description" {
   description = "OpenStack user description"
   type = string
-  default = "OpenStack User: ${var.os_usernames[count.index]}(${var.os_user_email_addr[count.index]})"
 }
 
 variable "os_user_default_password" {
@@ -35,5 +31,4 @@ variable "os_user_default_password" {
 variable "os_user_email_addr" {
   description = "OpenStack User email address"
   type = string
-  default = ["user1@example.com","user2@example.com"]
 }
