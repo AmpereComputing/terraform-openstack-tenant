23202dbbe996e319583941585909773d77d6075c
Peter Pouliot <peter@pouliot.net>
2020-10-06 16:43:17 +0000

    updating default quotas

---
+++ quota.tf
@@ -0,0 +1,9 @@
+resource "openstack_compute_quotaset_v2" "quotaset_1" {
+  project_id           = "${openstack_identity_project_v3.project_1.id}"
+  key_pairs            = 10
+  ram                  = 96000
+  cores                = 40 
+  instances            = 40
+  server_groups        = 4
+  server_group_members = 8
+}
