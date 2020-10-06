3961abe4cccef121ec444e088905044bfd513663
Peter Pouliot <peter@pouliot.net>
2020-10-06 14:21:16 +0000

    fixing missing character in var

--- users.tf
+++ users.tf
@@ -2,7 +2,7 @@
 resource "openstack_identity_user_v3" "user_1" {
   count = length(var.os_usernames)
   default_project_id = openstack_identity_project_v3.project_1.id
-  name               = var.os_username[count.index]
+  name               = var.os_usernames[count.index]
   description        = var_os_user_description
 
   password = var.os_user_default_password
