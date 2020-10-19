df724d5e06a1bee9778441d5602713473c294fdc
Peter Pouliot <peter@pouliot.net>
2020-10-19 07:20:56 -0700

    fixing missing "

--- security_group.tf
+++ security_group.tf
@@ -45,7 +45,7 @@ data "openstack_identity_project_v3" "project_1" {
   name = "var.os_project_name"
 }
 
-data "openstack_networking_secgroup_v2 "project_1" {
+data "openstack_networking_secgroup_v2" "project_1" {
   name = "${var.os_project_name} secgroup"
   tenant_id = "${openstack_identity_project_v3.project_1.id}"
 }
