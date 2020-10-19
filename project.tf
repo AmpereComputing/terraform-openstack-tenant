f88fd7453916c940b304d1ec5b014a4034bf242d
Peter Pouliot <peter@pouliot.net>
2020-10-19 10:49:38 -0700

    trying to cleanup error

--- project.tf
+++ project.tf
@@ -4,6 +4,16 @@ resource "openstack_identity_project_v3" "project_1" {
   description = "var.os_project_description"
 }
 
-#data "openstack_identity_project_v3" "project_1" {
-#  name = "var.os_project_name"
-#}
+data "openstack_identity_project_v3" "project_1" {
+  name = openstack_identitity_project_v3.project_1.name
+}
+
+resource "openstack_compute_quotaset_v2" "quotaset_1" {
+  project_id           = openstack_identity_project_v3.project_1.id
+  key_pairs            = 10
+  ram                  = 96000
+  cores                = 40 
+  instances            = 40
+  server_groups        = 4
+  server_group_members = 8
+}
