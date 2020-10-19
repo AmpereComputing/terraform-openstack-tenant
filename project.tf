47b1e76ffe759ed5b41baa4a4a1c86f2a43e1fde
Peter Pouliot <peter@pouliot.net>
2020-10-19 08:00:29 -0700

    removing sec group and usiung default

--- project.tf
+++ project.tf
@@ -3,3 +3,8 @@ resource "openstack_identity_project_v3" "project_1" {
   name        = "var.os_project_name"
   description = "var.os_project_description"
 }
+
+data "openstack_identity_project_v3" "project_1" {
+  name = "var.os_project_name"
+}
+
--- security_group.tf
+++ security_group.tf
@@ -41,62 +41,59 @@
 #  }
 #
 #}
-data "openstack_identity_project_v3" "project_1" {
-  name = "var.os_project_name"
-}
-
-data "openstack_networking_secgroup_v2" "project_1" {
-  name = "${var.os_project_name} secgroup"
-  tenant_id = openstack_identity_project_v3.project_1.id
-}
-
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_ssh" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 22
-  port_range_max    = 22
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.project_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_http" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 80
-  port_range_max    = 80
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.project_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_https" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 443
-  port_range_max    = 443
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.project_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_k8s" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "tcp"
-  port_range_min    = 8080
-  port_range_max    = 8080
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.project_1.id
-}
-
-resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_icmp" {
-  direction         = "ingress"
-  ethertype         = "IPv4"
-  protocol          = "icmp"
-  port_range_min    = -1
-  port_range_max    = -1
-  remote_ip_prefix  = "0.0.0.0/0"
-  security_group_id = openstack_networking_secgroup_v2.project_1.id
-}
+#
+#data "openstack_networking_secgroup_v2" "project_1" {
+#  name = "${var.os_project_name} secgroup"
+#  tenant_id = openstack_identity_project_v3.project_1.id
+#}
+#
+#
+#resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_ssh" {
+#  direction         = "ingress"
+#  ethertype         = "IPv4"
+#  protocol          = "tcp"
+#  port_range_min    = 22
+#  port_range_max    = 22
+#  remote_ip_prefix  = "0.0.0.0/0"
+#  security_group_id = openstack_networking_secgroup_v2.project_1.id
+#}
+#
+#resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_http" {
+#  direction         = "ingress"
+#  ethertype         = "IPv4"
+#  protocol          = "tcp"
+#  port_range_min    = 80
+#  port_range_max    = 80
+#  remote_ip_prefix  = "0.0.0.0/0"
+#  security_group_id = openstack_networking_secgroup_v2.project_1.id
+#}
+#
+#resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_https" {
+#  direction         = "ingress"
+#  ethertype         = "IPv4"
+#  protocol          = "tcp"
+#  port_range_min    = 443
+#  port_range_max    = 443
+#  remote_ip_prefix  = "0.0.0.0/0"
+#  security_group_id = openstack_networking_secgroup_v2.project_1.id
+#}
+#
+#resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_k8s" {
+#  direction         = "ingress"
+#  ethertype         = "IPv4"
+#  protocol          = "tcp"
+#  port_range_min    = 8080
+#  port_range_max    = 8080
+#  remote_ip_prefix  = "0.0.0.0/0"
+#  security_group_id = openstack_networking_secgroup_v2.project_1.id
+#}
+#
+#resource "openstack_networking_secgroup_rule_v2" "secgroup_default_rule_icmp" {
+#  direction         = "ingress"
+#  ethertype         = "IPv4"
+#  protocol          = "icmp"
+#  port_range_min    = -1
+#  port_range_max    = -1
+#  remote_ip_prefix  = "0.0.0.0/0"
+#  security_group_id = openstack_networking_secgroup_v2.project_1.id
+#}
