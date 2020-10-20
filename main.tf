989e0df3a5136c83d15a80269199769f91b5bc2e
Peter Pouliot <peter@pouliot.net>
2020-10-20 08:00:08 -0700

    removing unneeded "

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # Create OpenStack Project
 resource "openstack_identity_project_v3" "project_1" {
-  name        = "var.os_project_name"
-  description = "var.os_project_description"
+  name        = var.os_project_name
+  description = var.os_project_description
 }
 
 #data "openstack_identity_project_v3" "project_1" {
