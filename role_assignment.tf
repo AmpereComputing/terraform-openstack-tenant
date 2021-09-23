6d818a10d3cc0af137784c0b41fb8530199cf0a7
Peter Pouliot <peter@pouliot.net>
2021-09-23 14:37:51 -0400

    Update role_assignment.tf

--- role_assignment.tf
+++ role_assignment.tf
@@ -3,5 +3,5 @@ resource "openstack_identity_role_assignment_v3" "role_assignment_1" {
   count = length(var.os_usernames)
   user_id    = openstack_identity_user_v3.user_1[count.index].id
   project_id = openstack_identity_project_v3.project_1.id
-  role_id    = openstack_identity_role_v3.role_1.id
+  role_id    = openstack_identity_role_v3.member.id
 }
