253e74605e0796231b46a1936e0c1fc60f457420
Peter Pouliot <peter@pouliot.net>
2021-09-23 14:57:35 -0400

    Update main.tf
    Get ids from output and set to local vars

--- main.tf
+++ main.tf
@@ -7,3 +7,18 @@ resource "openstack_identity_project_v3" "project_1" {
 #data "openstack_identity_project_v3" "project_1" {
 #  name = openstack_identitity_project_v3.project_1.name
 #}
+locals {
+    openstack_member_role_id = data.openstack_identity_role_v3.member.id
+    openstack_admin_role_id = data.openstack_identity_role_v3.admin.id
+    openstack_reader_role_id = data.openstack_identity_role_v3.reader.id
+}
+
+output "local_openstack_member_role_id" {
+  value = local.openstack_member_role_id
+}
+output "local_openstack_admin_role_id" {
+  value = local.openstack_admin_role_id
+}
+output "local_openstack_reader_role_id" {
+  value = local.openstack_reader_role_id
+}
