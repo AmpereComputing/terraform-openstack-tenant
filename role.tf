c42e9d281a7d2154fd643584853e93cbc1e467f1
Peter Pouliot <peter@pouliot.net>
2021-09-23 14:47:31 -0400

    Update role.tf

--- role.tf
+++ role.tf
@@ -2,3 +2,14 @@
 # resource "openstack_identity_role_v3" "role_1" {
 # name = var.os_role_name
 #}
+# Create OpenStack Global Roles
+data "openstack_identity_role_v3" "member" {
+  name = "member"
+}
+data "openstack_identity_role_v3" "admin" {
+  name = "admin"
+}
+
+data "openstack_identity_role_v3" "reader" {
+  name = "reader"
+}
